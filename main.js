3b1924bedf350136bd8def9dfadc636861b44a2d
Pasha006 <91893567+Yan3330@users.noreply.github.com>
2022-01-15 11:59:44 +0200

    abcdeshka

---
+++ main.js
@@ -0,0 +1,92 @@
+// let a = 'sdfsdf'
+// let b = 1235
+// let c = false
+// let d = 125n
+// let e = null
+// let f
+// let g = '69' - '25'
+
+// alert(typeof(Boolean(a)))
+// alert(a)
+// alert(typeof(String(b)))
+// alert(b)
+// alert(typeof(Number(c)))
+// alert(c)
+// alert(typeof(String(d)))
+// alert(d)
+// alert(typeof(Boolean(e)))
+// alert(e)
+// alert(typeof(String(f)))
+// alert(f)
+// alert(typeof(String(g)))
+// alert(g)
+
+
+
+
+
+// 15 
+
+// let s = prompt('S круга=')
+// let d = Math.sqrt(4 * s / 3.14)
+// let l = 3.14 * d
+// alert(`Диаметр круга=${d}, длина окружности=${l}.`)
+
+// let r1 = prompt('r1=')
+// let r2 = prompt('r2=')
+// let distance = Math.abs(r2-r1)
+// alert(`Расстояние между двумя точками на числовой оси=${distance}`)
+
+// let A = prompt('A=')
+// let B = prompt('B=')
+// let C = prompt('C=')
+// let distAC = Math.abs(C-A)
+// let distBC = Math.abs(C-B)
+// let sum = distAC + distBC
+// alert(`Длина АС=${distAC}, длина ВС=${distBC}, сумма этих отрезков=${sum}`)
+
+// let A = prompt('A=')
+// let B = prompt('B=')
+// let C = prompt('C=')
+// let AC = Math.abs(C-A)
+// let BC = Math.abs(C-B)
+// let product = AC * BC
+// alert(`Длина АС=${AC}, длина ВС=${BC}, произведение этих отрезков=${product}`)
+
+// let x1 = prompt('Координата x1=')
+// let y1 = prompt('Координата y1=')
+// let x2 = prompt('Координата x2=')                            
+// let y2 = prompt('Координата y2=')
+// let dista = Math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+// let x3 = prompt('Координата x3=')
+// let y3 = prompt('Координата y3=')
+// let distb = Math.sqrt((x3 - x3)**2 + (y3 - y3)**2)
+// let p = (dista + distb) * 2
+// let s = distb * dista
+// alert(`Периметр прямоугольника=${p}, площадь прямоугольника=${s}`)
+
+// let x1 = prompt('Координата x1=')
+// let y1 = prompt('Координата y1=')
+// let x2 = prompt('Координата x2=')                          
+// let y2 = prompt('Координата y2=')
+// let dist = Math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+// alert(`Расстояние между двумя точками=${dist}`)
+
+                                                                         //Integer
+                                                                         //1
+// let lsm = prompt('Введите расстояние в см')
+// let lm = Math.floor(lsm / 100)
+// alert(`Количество полных метров в этом расстоянии=${lm}`)
+
+// let mkg = prompt(`Введите массу в кг`)
+// let mt = Math.floor(mkg / 1000)
+// alert(`Количество полных тонн в этой массе=${mt}`)
+
+// let sizebyte = prompt(`Введите размер файла в байтах`)
+// let sizekb = Math.floor(sizebyte / 1024)
+// alert(`Количество полных килобайтов, которые занимает данный файл=${sizekb}`)
+
+// let a = prompt('Введите число а(a>b)')
+// let b = prompt('Введите число b(a>b)')
+// let nume = Math.floor(a / b)
+// alert(`Количество отрезков b, размещенных на отрезке а=${nume}`)
